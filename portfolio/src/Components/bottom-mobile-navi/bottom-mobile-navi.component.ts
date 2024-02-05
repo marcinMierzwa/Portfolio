6b608461cbd9d2c1a0b2e9c85f634e4ebb9fefa5
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-02-05 19:05:31 +0100

    update home component

---
+++ portfolio/src/Components/bottom-mobile-navi/bottom-mobile-navi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-mobile-navi',
+  standalone: true,
+  imports: [],
+  templateUrl: './bottom-mobile-navi.component.html',
+  styleUrl: './bottom-mobile-navi.component.css'
+})
+export class BottomMobileNaviComponent {
+
+}
--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "../Components/navigation/navigation.component";
 import { FooterComponent } from "../Components/footer/footer.component";
 import { SideBarComponent } from "../Components/side-bar/side-bar.component";
+import { BottomMobileNaviComponent } from "../Components/bottom-mobile-navi/bottom-mobile-navi.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavigationComponent, FooterComponent, SideBarComponent, NavigationComponent]
+    imports: [CommonModule, RouterOutlet, NavigationComponent, FooterComponent, SideBarComponent, NavigationComponent, BottomMobileNaviComponent]
 })
 export class AppComponent {
   title = 'portfolio';
