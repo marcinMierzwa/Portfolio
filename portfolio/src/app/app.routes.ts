c524b0fa44fe63ffb3a700c712548a2943098bd6
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-02-19 22:55:52 +0100

    create about component

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../Components/home/home.component';
+import { AboutComponent } from '../Components/about/about.component';
 
 export const routes: Routes = [
 {
@@ -10,6 +11,7 @@ export const routes: Routes = [
 },
 {
     path: 'home',
-    component: HomeComponent,
+    // component: HomeComponent,
+    component: AboutComponent,
 }
 ];
