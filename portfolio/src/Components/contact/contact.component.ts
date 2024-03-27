47ed75929009da51773fb8225496e986a3800c55
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-03-27 19:01:13 +0100

    update form

--- portfolio/src/Components/contact/contact.component.ts
+++ portfolio/src/Components/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormComponent } from "../form/form.component";
 
 @Component({
-  selector: 'app-contact',
-  standalone: true,
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+    selector: 'app-contact',
+    standalone: true,
+    templateUrl: './contact.component.html',
+    styleUrl: './contact.component.css',
+    imports: [FormComponent]
 })
 export class ContactComponent {
 
