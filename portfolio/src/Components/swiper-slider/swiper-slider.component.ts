c0622d2cabb6887ed75e458e50a0b250e6959595
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-04-17 18:43:13 +0200

    add pagination

--- portfolio/src/Components/swiper-slider/swiper-slider.component.ts
+++ portfolio/src/Components/swiper-slider/swiper-slider.component.ts
@@ -11,11 +11,9 @@ import { Observable } from 'rxjs';
 import { SwiperContainer } from 'swiper/element/bundle';
 import { DataService, Project } from '../../Services/data.service';
 import { CommonModule } from '@angular/common';
-import { SwiperOptions} from 'swiper/types';
+import { SwiperOptions } from 'swiper/types';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
-registerSwiperElements()
-
-
+registerSwiperElements();
 
 @Component({
   selector: 'app-swiper-slider',
@@ -25,51 +23,53 @@ registerSwiperElements()
   templateUrl: './swiper-slider.component.html',
   styleUrl: './swiper-slider.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
-
 })
 export class SwiperSliderComponent implements OnInit {
 
-  data:DataService = inject(DataService);
+  data: DataService = inject(DataService);
 
   projects$ = this.data.getProjects as Observable<Project[]>;
 
   swiperElement = signal<SwiperContainer | null>(null);
 
   ngOnInit(): void {
-
-    const swiperElementConstructor = document.querySelector('swiper-container');
+    const swiperElementConstructor = document.querySelector('.swiper');
 
     const swiperOptions: SwiperOptions = {
 
-      slidesPerView: 'auto',
       // loop: true,
-      pagination: {
-        clickable: true,
-        // dynamicBullets: true,
-        bulletClass: 'swiper-pagination-bullet',
-      },
 
-
-      effect: "cube",
+      effect: 'cube',
       cubeEffect: {
         shadow: true,
         slideShadows: true,
         shadowOffset: 40,
         shadowScale: 0.8,
-    },
-    allowTouchMove: true,
-    navigation: {
-      prevEl: '#prev',
-      nextEl: '#next',
-    },
+      },
+      allowTouchMove: true,
+      navigation: {
+        prevEl: '#prev',
+        nextEl: '#next',
+      },
+      pagination: {
+        // renderBullet: function(index, className) {
+        //   return '<span class="' + className + '">' + (index + 1) + '</span>';
+        // },
+        clickable: true,
+        dynamicBullets: true,
+        dynamicMainBullets: 2,
+      },
 
-  }
-
+    };
     Object.assign(swiperElementConstructor!, swiperOptions);
 
     this.swiperElement.set(swiperElementConstructor as SwiperContainer);
 
     this.swiperElement()?.initialize();
+
+   
+
   }
+
 
 }
